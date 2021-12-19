912aa7cc2cecda814f25dec6041b18108b784464
NyaNyak <chsgood1004@gmail.com>
2021-12-19 16:52:45 +0900

    욕설 필터링 기능

--- chatmanage.py
+++ chatmanage.py
@@ -1,12 +1,14 @@
 import discord
 
-shit = ["시발", "씨발", "애미", "느금"]
+shit = ["시발", "씨발", "애미", "느금", "병신", "개새끼"]
 
-def filtering(message):
+def filtering(client):
     @client.event
     async def on_message(message):
         chat = message.content
         for i in shit:
             if i in chat:
-                await message.channel.send("소환사님 예쁜 말^^을 사용해^^주세요~!")
+                await message.channel.send("소환사님 🎀✨예쁜 말✨🎀을 사용해😡주세요~!😎😘")
                 await message.delete()
+
+
