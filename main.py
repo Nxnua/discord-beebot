bc057beccf317f3d61fcbcd99b5da767f7322f6f
LimitofIntegral <ufshg@naver.com>
2021-12-20 23:29:02 +0900

    길드조인 수정 3

--- main.py
+++ main.py
@@ -32,9 +32,9 @@ async def change_status():
 
 @client.event
 async def on_guild_join(ctx):
-    await ctx.send('안녕하세요 소환사님, 꿀벌봇이 왔어요🐝')
-    await ctx.send('도움이 필요하시다면 아래 명령을 내려주세요!')
-    await ctx.send(embed=command())
+    await ctx.channel.send('안녕하세요 소환사님, 꿀벌봇이 왔어요🐝')
+    await ctx.channel.send('도움이 필요하시다면 아래 명령을 내려주세요!')
+    await ctx.channel.send(embed=command())
 
 
 @client.event
